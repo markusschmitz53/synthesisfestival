75d7bc521883d7f1fff6018d1f570def682c98d0
Markus Schmitz <18517685+markusschmitz53@users.noreply.github.com>
2022-02-04 14:25:11 +0100

    Fix 404 page (update content)

--- wp-content/themes/synthesis/404.php
+++ wp-content/themes/synthesis/404.php
@@ -2,11 +2,10 @@
 <main id="content" role="main">
 <article id="post-0" class="post not-found">
 <header class="header">
-<h1 class="entry-title" itemprop="name"><?php esc_html_e( 'Not Found', 'blankslate' ); ?></h1>
+<h1 class="entry-title has-text-align-center" itemprop="name"><?php esc_html_e( 'Oops', 'blankslate' ); ?></h1>
 </header>
 <div class="entry-content" itemprop="mainContentOfPage">
-<p><?php esc_html_e( 'Nothing found for the requested page. Try a search instead?', 'blankslate' ); ?></p>
-<?php get_search_form(); ?>
+	<p class="has-text-align-center"><?php esc_html_e( 'Diese Seite gibt es nicht. ', 'blankslate' ); ?> <a href="https://synthesis-festival.de/">Zur Startseite</a></p>
 </div>
 </article>
 </main>
