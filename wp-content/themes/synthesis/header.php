9a66e620678b377bbc06e04a5afd92d0fcee099f
Markus Schmitz <18517685+markusschmitz53@users.noreply.github.com>
2022-02-21 13:34:07 +0100

    Update description

--- wp-content/themes/synthesis/header.php
+++ wp-content/themes/synthesis/header.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
-<meta name="description" content="Welcome to the year 2437. Welcome to Synthesis Festival." />
+<meta name="description" content="Synthesis Festival. The playground for your inner child. 21-24 July 2022, close to Berlin" />
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <?php wp_head(); ?>
 </head>
